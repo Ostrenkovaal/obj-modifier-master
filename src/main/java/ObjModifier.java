a6cc96300ae52ccc224fe37bc7f9b18c6b27c893
ostrenkovaal <lokki910@yandex.ru>
2019-11-01 17:53:41 +0300

    #get a pivot through finding the center of weight of the points

--- src/main/java/ObjModifier.java
+++ src/main/java/ObjModifier.java
@@ -4,12 +4,12 @@ public class ObjModifier {
 
     public static void centerVertices(FloatBuffer buf) {
         buf.clear();
-        Vector pivotValue = getPivotValue(buf);
-        transferFigure(buf, pivotValue);
+        Vector pivot = getPivotValue(buf);
+        transferFigure(buf, pivot);
         buf.rewind();
     }
 
-    //declare class Vector for convenient interaction with point data
+    //declare class Vector for convenient interaction with vertex data
     //similar classes exist in external pluggable libraries for 3d rendering
     private static class Vector {
         public float xAxisValue;
@@ -19,69 +19,28 @@ public class ObjModifier {
         public Vector() {
         }
 
-        public Vector(float xAxisValue, float yAxisValue, float zAxisValue) {
-            this.xAxisValue = xAxisValue;
-            this.yAxisValue = yAxisValue;
-            this.zAxisValue = zAxisValue;
-        }
-
-        public Vector addEndPoint(Vector vector) {
-            xAxisValue += vector.xAxisValue;
-            yAxisValue += vector.yAxisValue;
-            zAxisValue += vector.zAxisValue;
+        public Vector getPivot(float factor) {
+            xAxisValue /= factor;
+            yAxisValue /= factor;
+            zAxisValue /= factor;
             return this;
         }
-
-        public Vector multiply(float factor) {
-            xAxisValue *= factor;
-            yAxisValue *= factor;
-            zAxisValue *= factor;
-            return this;
-        }
-
     }
 
-    //pivot point search method on a figure
-    private static Vector getPivotValue(FloatBuffer buf){
+    //pivot point search method on a figure through search weight centers for figure
+    private static Vector getPivotValue(FloatBuffer buf) {
 
-        Vector minValue = new Vector(Float.POSITIVE_INFINITY, Float.POSITIVE_INFINITY, Float.POSITIVE_INFINITY);
-        Vector maxValue = new Vector(Float.NEGATIVE_INFINITY, Float.NEGATIVE_INFINITY, Float.NEGATIVE_INFINITY);
-        Vector bufValue = new Vector();
-
+        //calculate weight points
+        Vector weightPolygon = new Vector();
         for (int i = 0; i < buf.limit(); ) {
-            bufValue.xAxisValue = buf.get(i++);
-            bufValue.yAxisValue = buf.get(i++);
-            bufValue.zAxisValue = buf.get(i++);
-            findExtremes(minValue, maxValue, bufValue);
+            weightPolygon.xAxisValue += buf.get(i++);
+            weightPolygon.yAxisValue += buf.get(i++);
+            weightPolygon.zAxisValue += buf.get(i++);
         }
 
         //calculate model pivot as center of bounding box
-        Vector vertex = minValue.addEndPoint(maxValue);
-        Vector pivot = vertex.multiply(0.5f);
-
+        Vector pivot = weightPolygon.getPivot(buf.limit() / 3);
         return pivot;
-    }
-
-    //search for extrema on coordinate axes
-    private static void findExtremes(Vector minValue, Vector maxValue, Vector bufValue) {
-        if (bufValue.xAxisValue < minValue.xAxisValue) {
-            minValue.xAxisValue = bufValue.xAxisValue;
-        }
-        if (bufValue.xAxisValue > maxValue.xAxisValue) {
-            maxValue.xAxisValue = bufValue.xAxisValue;
-        }
-        if (bufValue.yAxisValue < minValue.yAxisValue) {
-            minValue.yAxisValue = bufValue.yAxisValue;
-        }
-        if (bufValue.yAxisValue > maxValue.yAxisValue) {
-            maxValue.yAxisValue = bufValue.yAxisValue;
-        }
-        if (bufValue.zAxisValue < minValue.zAxisValue) {
-            minValue.zAxisValue = bufValue.zAxisValue;
-        }
-        if (bufValue.zAxisValue > maxValue.zAxisValue) {
-            maxValue.zAxisValue = bufValue.zAxisValue;
-        }
     }
 
     //put in the buffer the values ​​of the points of the transfered figure
@@ -95,6 +54,4 @@ public class ObjModifier {
             buffer.put(buffer.get(i++) - pivot.zAxisValue);
         }
     }
-
 }
-
